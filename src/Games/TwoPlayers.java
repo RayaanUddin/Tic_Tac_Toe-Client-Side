eee0db2cd1c25be87144104269f2cb710347c18d
Rayaan Uddin <rayaan.uddin@outlook.com>
2024-09-29 22:54:01 +0100

    Logic for 2 player

---
+++ src/Games/TwoPlayers.java
@@ -0,0 +1,27 @@
+package Games;
+
+import UI.CellButton;
+import UI.GameFrame;
+
+public class TwoPlayers extends GameFrame {
+    char turn = 'o';
+
+    public TwoPlayers(int size) {
+        super(size);
+    }
+
+    @Override
+    public boolean onCellClick(CellButton cell) {
+        cell.setText(Character.toString(turn));
+        switch (turn) {
+            case 'o': turn = 'x'; break;
+            case 'x': turn = 'o'; break;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        new TwoPlayers(3);
+    }
+
+}
